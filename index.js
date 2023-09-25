63edcf8053577ddc51cb9daf5092d10b8a5a8682
CKeller2K <145724542+CKeller2K@users.noreply.github.com>
2023-09-25 00:59:58 -0700

    Update index.js
    updating github file with my work, runs with no errors on my side
    to run: open terminal at project directory, and run 'npm install pdfkit' without the single-quotes

--- index.js
+++ index.js
@@ -1,18 +1,68 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKeys.json");
+//dunno what these do LOL
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKeys.json");
 const { QuerySnapshot } = require("@google-cloud/firestore");
+const PDFDoc = require('pdfkit');
+const fs = require('fs');
 
+
+//gets service key and logs in as admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 
+//db is a database constant
 const db = admin.firestore();
-let documentRef = db.collection("INSERT-FIRESTORE-COLLECTION-HERE");
+//documentRef is a (currently) hard-coded collection from database
+const documentRef = db.collection("coleTest");
 
-documentRef.get().then((QuerySnapshot) => {  //learn a little javascript, => operator returns the next braces like a function
-    QuerySnapshot.forEach(document => {
-        console.log(document.data());   //seems to log like a python dictionary in key-value pairs
-    })
-})
+
+//'snapshot's' collection
+documentRef.get().then((QuerySnapshot) => {
+  //loops through every document in collection, document can be any name, usually doc
+  QuerySnapshot.forEach(document => {
+
+    //do stuff to document data here
+
+    //TODO find less messy way to get data
+    //document.data() returns firestore doc as map
+    //console.log(document.data());
+
+    //checks: if processed tag exists and is true, then does not create or open new pdf
+    // can use "document.hasOwnProperty('processed')" to check if tag exists, but no error if you just try to check value
+    if (document.data()['processed'] === true) {
+      //console.log("Already processed.");
+      return;
+    }
+
+    //doc var is new pdf
+    let doc = new PDFDoc();
+    //open doc for editing: createWriteStream takes a string input for file name; here name made with client name + order number
+    doc.pipe(fs.createWriteStream(`${document.data()['user_data']['name']}${document.data()['order']['order_number']}.pdf`));
+
+    //print title
+    doc.fontSize(16).text('Order\n', {align: 'center'});
+
+    //print data
+    //can use \n or doc.moveDown() to create lines, movedown takes spacing ex: doc.moveDown(.5)
+    doc.font('Times-Italic');
+    doc.fontSize(12).text(`
+    Subtotal: ${document.data()['order']['subtotal']}\n
+    Tax (8.25%): ${document.data()['order']['subtotal'] * 0.0825}\n
+    Total: ${document.data()['order']['total']}\n\n
+    Date: ${document.data()['order']['date']} || Order #: ${document.data()['order']['order_number']}
+    `, {align: 'left'});
+
+    //close pdf
+    doc.end();
+
+    //updates document with timestamp and 'processed' key + sets to true (creates key if not found)
+    // COMMENTED FOR REPEATED TESTS
+    //const res = await documentRef.update({timestamp: FieldValue.serverTimestamp(), processed: true});
+    });
+
+  //tries to catch errors AT GET SNAPSHOT, NOT DOCUMENT
+  }).catch((error) => {
+  console.log("Error at document read: ", error);
+});
