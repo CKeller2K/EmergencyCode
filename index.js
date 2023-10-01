fbeac17f810072c0c75d4c6432b7dc83ec84b4e9
CKeller2K <145724542+CKeller2K@users.noreply.github.com>
2023-10-01 16:09:46 -0700

    Update index.js
    -updated main document get block to also wait for new documents added
    -moved subprocesses to functions above main document-get
    -added new sendgrid functionality
    
    Please check modules.txt file to ensure all required modules are installed before running.

--- index.js
+++ index.js
@@ -1,68 +1,120 @@
-//dunno what these do LOL
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKeys.json");
-const { QuerySnapshot } = require("@google-cloud/firestore");
+const serviceAccount = require("./serviceAccKey.json");
+const firebase = require('firebase-admin');
 const PDFDoc = require('pdfkit');
 const fs = require('fs');
+const sendGrid = require('@sendgrid/mail');
 
 
-//gets service key and logs in as admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-
-//db is a database constant
+const projectId = 'csc-131-project-398318';
 const db = admin.firestore();
-//documentRef is a (currently) hard-coded collection from database
-const documentRef = db.collection("coleTest");
+const collectionRef = db.collection("coleTest");
+const storage = new Storage();
+
+//TODO fix .env file to import key without hardcoding
+//sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+sendGrid.setApiKey('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');
 
 
-//'snapshot's' collection
-documentRef.get().then((QuerySnapshot) => {
-  //loops through every document in collection, document can be any name, usually doc
-  QuerySnapshot.forEach(document => {
+function processDocument (document){
+  var fileName = 'N/A';
 
-    //do stuff to document data here
+  //data type #1; firestore doc has 4 sub divisions: client, host, order, and template
+  if (document.data()['template'] === 0) {
+    const client = document.data()['user_data'];
+    const host = document.data()['shop_data'];
+    const order = document.data()['order'];
 
-    //TODO find less messy way to get data
-    //document.data() returns firestore doc as map
-    //console.log(document.data());
-
-    //checks: if processed tag exists and is true, then does not create or open new pdf
-    // can use "document.hasOwnProperty('processed')" to check if tag exists, but no error if you just try to check value
-    if (document.data()['processed'] === true) {
-      //console.log("Already processed.");
-      return;
-    }
-
-    //doc var is new pdf
     let doc = new PDFDoc();
     //open doc for editing: createWriteStream takes a string input for file name; here name made with client name + order number
-    doc.pipe(fs.createWriteStream(`${document.data()['user_data']['name']}${document.data()['order']['order_number']}.pdf`));
+    fileName = `${client['name']}${order['order_number']}.pdf`;
+    doc.pipe(fs.createWriteStream(`${client['name']}${order['order_number']}.pdf`));
 
-    //print title
     doc.fontSize(16).text('Order\n', {align: 'center'});
 
     //print data
     //can use \n or doc.moveDown() to create lines, movedown takes spacing ex: doc.moveDown(.5)
     doc.font('Times-Italic');
     doc.fontSize(12).text(`
-    Subtotal: ${document.data()['order']['subtotal']}\n
-    Tax (8.25%): ${document.data()['order']['subtotal'] * 0.0825}\n
-    Total: ${document.data()['order']['total']}\n\n
-    Date: ${document.data()['order']['date']} || Order #: ${document.data()['order']['order_number']}
+    Subtotal: ${order['subtotal']}\n
+    Tax (8.25%): ${order['subtotal'] * 0.0825}\n
+    Total: ${order['total']}\n\n\n
+    Date: ${order['date']} || Order #: ${order['order_number']}\n
+    Sending Reciept to ${client['email']}
     `, {align: 'left'});
+    if (! client['reward_member']) doc.text(`\n\nConsider joining the rewards program!`, {align: 'center'});
 
-    //close pdf
     doc.end();
+  }
 
-    //updates document with timestamp and 'processed' key + sets to true (creates key if not found)
-    // COMMENTED FOR REPEATED TESTS
-    //const res = await documentRef.update({timestamp: FieldValue.serverTimestamp(), processed: true});
+  //data type #2; no sub divisions -> all in document data map
+  else if (document.data()['template'] === 1){
+  }
+  
+  return fileName;
+}
+
+
+//takes filename, sends that file to hardcoded email
+//TODO figure out how to get email from processDocument, add argument to emailDoc
+function emailDocument(fileName){
+  const attachmentBuffer = fs.readFileSync(fileName);
+  const attachmentBase64 = attachmentBuffer.toString('base64');
+
+  const message = {
+    to: 'YOUR EMAIL HERE',     //TO EMAIL HARDCODED, CHANGE TO YOURS HERE
+    from: 'CSC131.emergency.code@gmail.com',
+    subject: 'Processed File Sent',
+    text: 'Check PDF is attached. Do not reply to this email.',
+    attachments: [
+      {
+        filename: fileName,
+        content: attachmentBase64, 
+        type: 'application/pdf',
+        disposition: 'attachment'
+      }
+    ]
+  };
+
+  sendGrid.send(message)
+    .catch((error) => {
+      console.error(error);
     });
+}
 
-  //tries to catch errors AT GET SNAPSHOT, NOT DOCUMENT
-  }).catch((error) => {
-  console.log("Error at document read: ", error);
+
+
+  
+
+//processing loop waits for collection 'change' and processes added documents
+//also attempts to process all existing documents
+collectionRef.onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type !== 'added') return;
+    else if (change.doc.data()['processed'] === true) return;
+
+
+    //TODO GET + REFERENCE TEMPLATE -> format for processDocument()
+
+
+    //func processes doc into a pdf file using data gotten above
+    // TODO function call not working as intended with new documents
+    let fileName = processDocument(change.doc);
+
+    
+    //TODO store output to bucket
+
+    
+    //func sends pdf attachment via email to client_email !! COMMENTED BECAUSE IT SENDS AN EMAIL EVERY RUN FOR EVERY DOC !!
+    //TODO fix email pass from processDoc
+    emailDocument(fileName);
+
+    
+    //updates document with processed key and sets to true (creates key if not found)
+    //change.doc.ref.update({processed: true, timeProcessed: firebase.firestore.Timestamp.now()});
+    });
 });
