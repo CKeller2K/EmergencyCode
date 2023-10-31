27f98a996f92deee80b3933aa3950a39ade086c7
CKeller2K <145724542+CKeller2K@users.noreply.github.com>
2023-10-30 17:10:08 -0700

    Updating localIndex for Demo #3
    Let me know if there are any problems through @Cole in the discord.
    Expects templating like James' work (available in storage pdf-json-buckets -> test_template_3.tex)
    and expects input formatting like my work (available in firestore -> coleTest -> T8KDdnLqAGdgZUyv8kAx).

--- localindex.js
+++ localindex.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
 const latex = require("node-latex");
+const Handlebars = require('handlebars');
 const fs = require("fs");
 const fsp = require('fs/promises');
 const sendGrid = require('@sendgrid/mail');
@@ -21,11 +22,13 @@ collectionRef.onSnapshot((snapshot) => {
     const docRef = change.doc.ref;
     const data = change.doc.data();
     const docID = change.doc.id;
+    var resultBool = false;
 
     console.log(`Document triggered: ${change.doc.id}...`);
 
     //TODO work on checking data to remove docs that don't meet basics
-    if (!data.hasOwnProperty('metaData')) {
+    resultBool = (!data.hasOwnProperty('metaData'));
+    if (resultBool) {
       console.log ("Improper document structure. Expected document information in metaData object.");
       return null;
     }
@@ -33,7 +36,8 @@ collectionRef.onSnapshot((snapshot) => {
     const inBucketName = metaData.inBucket;
     const outBucketName = metaData.outBucket;
 
-    if (metaData.state !== "create") {
+    resultBool = metaData.state !== "create";
+    if (resultBool) {
       console.log("Update trigger 'create' not detected. Change 'state' variable to begin processing.");
       return null;
     }
@@ -47,91 +51,99 @@ collectionRef.onSnapshot((snapshot) => {
     // specify template file IN BUCKET using template data; remember template var needs to include extension :)
     const inFile = inBucket.file(`${metaData.template}`);
 
-    if (
-      inFile.exists().catch(function(err) {
-        console.log(err);
-        return true;
-    }))
-    return docRef.update({state: "ERROR", ERROR: `Error: Template does not exist: ${metaData.template}`, timeProcessed: admin.firestore.FieldValue.serverTimestamp()});
+    await inFile.exists()
+      .catch(function(err) {
+          console.log(err);
+          resultBool = true;
+      })
+      //.catch catches promise errors, .then is where file exists bool is sent; data is obj type, duno why
+      .then(function(data) {
+        if (data[0]) resultBool = false;
+        else resultBool = true;
+    });
+    if (resultBool)
+    return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: Template does not exist: ${metaData.template}`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
     else console.log("Template found in bucket.");
 
-    //get template from bucket; same as onCreate: see notes there
-    const downloading = inFile.download({destination: inFilePath});
-    if (downloading.err) {  //TODO currently not sure if this catches errors... google hasn't error'd yet :)
-      console.log(downloading.err);
-      return docRef.update({state: "ERROR", ERROR: downloading.err, timeProcessed: admin.firestore.FieldValue.serverTimestamp()});
-    }
+    //get template from bucket
+    await inFile.download({destination: inFilePath})
+      .catch(function(err) {
+        console.log(err);
+        resultBool = true;
+      })
+      .then(function(data) {
+        //TODO figure out what's in data and update resultbool correctly. should include success or fail data.
+        resultBool = false;
+    });
+    if (resultBool)
+    return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: Download failed for destination: ${inFilePath}`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
     else console.log(`No error downloading. Sent to ${inFilePath}`);
 
 
-    //kenny code to process, expects tex file exists in /tmp/ subdirectory; after done, pdf expected in /tmp/ subdirectory
+    //read handlebars tex file from local dir
+    //expects tex file exists in /tmp/ subdirectory; after done, pdf expected in /tmp/ subdirectory
     const originalData = await fsp.readFile(inFilePath, { encoding: 'utf8' });
-    originalData.on('error', (error) => {
-      console.log(`Error reading download: ${error}`);
-      return docRef.update({state: "ERROR", ERROR: error, timeProcessed: admin.firestore.FieldValue.serverTimestamp()});
-    });
+    /*.catch(function(err) {
+        console.log(err);
+        resultBool = true;
+    }).then(() => {
+        resultBool = false;
+    });*/
+    if (resultBool)
+    return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: Reading downloaded template failed: ${inFilePath}`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
+    else console.log('No error reading download. Modifying...');
 
-    console.log('No error reading download. Modifying...');
+    //read tex document into handlebars functional string
+    //TODO error handling lmao; what does Handlebars.compile return?? the second line????
+    const handlebarsTemplate = Handlebars.compile(originalData);
+    //compile handlebars with doc data pass-through -> traditional latex doc
+    const latexDocument = handlebarsTemplate(data);
 
-    const itemMap0 = data.items[0];
-    const itemMap1 = data.items[1];
-    const itemMap2 = data.items[2];
-
-    let modifiedData = originalData;
-    modifiedData = modifiedData.replace('<<customerName>>', data.client_name);
-    modifiedData = modifiedData.replace('<<customerPhone>>', 'N/A');
-    modifiedData = modifiedData.replace('<<customerAddress>>', data.client_address);
-    modifiedData = modifiedData.replace('<<customerEmail>>', data.client_email);
-
-    modifiedData = modifiedData.replace('<<companyName>>', data.host_name);
-    modifiedData = modifiedData.replace('<<companyPhone>>', 'N/A');
-    modifiedData = modifiedData.replace('<<companyAddress>>', data.host_location);
-    modifiedData = modifiedData.replace('<<companyEmail>>', 'N/A');
-
-    modifiedData = modifiedData.replace('<<invoiceNumber>>', 'N/A');
-    modifiedData = modifiedData.replace('<<subTotal>>' , data.order_subtotal);
-    modifiedData = modifiedData.replace('<<taxAmount>>' , data.order_tax);
-    modifiedData = modifiedData.replace('<<totalAmount>>' , data.order_total);
-    
-    modifiedData = modifiedData.replace('<<product1Name>>' , itemMap0.name);
-    modifiedData = modifiedData.replace('<<product1Cost>>' , itemMap0.cost);
-    modifiedData = modifiedData.replace('<<product1Amount>>' , itemMap0.count);
-
-    modifiedData = modifiedData.replace('<<product2Name>>' , itemMap1.name);
-    modifiedData = modifiedData.replace('<<product2Cost>>' , itemMap1.cost);
-    modifiedData = modifiedData.replace('<<product2Amount>>' , itemMap1.count);
-
-    modifiedData = modifiedData.replace('<<product3Name>>' , itemMap2.name);
-    modifiedData = modifiedData.replace('<<product3Cost>>' , itemMap2.cost);
-    modifiedData = modifiedData.replace('<<product3Amount>>' , itemMap2.count);
-
+    //TODO? can save completed tex file to ./tmp/ if need be
+    // await fsp.writeFile(`${docId}.tex`, latexDocument);  //also needs error handling lol
       
     console.log('Template modified with doc data.');
 
+    //run pdflatex to compile latexDoc string to PDF file in ./tmp/ folder
     const options = { inputs: ['.', 'TeXworks'] };
-    const outPDF = latex(modifiedData, options);  //TODO no idea how to error catch this. at the moment this always stops program!!!
+    const outPDF = latex(latexDocument, options);  //TODO no idea how to error catch this. error at the moment this always stops program!!!
     console.log(`No error running PDFLatex.`);
 
     await outPDF.pipe(fs.createWriteStream(outFilePath))
     .on('error', (error) => {
-      console.log(`Pipe error: ${error}`)
-      return docRef.update({state: "ERROR", ERROR: error, timeProcessed: admin.firestore.FieldValue.serverTimestamp()});
+      console.log(`Write-stream pipe error: ${error}`)
+      resultBool = true;
+      return null;
     })
     .on('finish', async () => {
       console.log('PDF generated successfully.');
 
       //upload finished pdf to processed bucket
-      const uploading = await outBucket.upload(outFilePath);
-      if (uploading.err) {
-        console.log(`Error uploading PDF: ${uploading.err}`);
-        return docRef.update({state: "ERROR", ERROR: uploading.err, timeProcessed: admin.firestore.FieldValue.serverTimestamp()});
-      }
+      await outBucket.upload(outFilePath)
+      .catch(function(err) {
+        console.log(err);
+        resultBool = true;
+      }).then(function(data){
+        //SEE DOWNLOAD BLOCK, UPLOAD COULD STILL FAIL IF HERE
+        resultBool = false;
+      });
+      if (resultBool)
+      return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: PDF upload failed: ${outFilePath} -> ${outBucketName}`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
       else console.log(`No error uploading: ${docID}.pdf sent to ${outBucketName}`);
 
 
       console.log(`Sending Email.`);
 
-      var attachmentBuffer = await fsp.readFile(outFilePath, { encoding: 'base64' });
+      var attachmentBuffer = await fsp.readFile(outFilePath, { encoding: 'base64' }); //TODO HERE AND ORIGINALDATA GET catching error from promise fucks with saving data
+      /*.catch(function(err) {
+        console.log(err);
+        resultBool = true;
+      }).then(() => {
+        resultBool = false;
+    });*/
+    if (resultBool) //TODO big error here. returns for the .on(finish) of original read. return does not end doc execution
+    return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: Failed reading PDF to Base-64: ${outFilePath}`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
+    else console.log('No error reading download. Modifying...');
 
       const message = {
         to: process.env.EMAIL,
@@ -151,13 +163,23 @@ collectionRef.onSnapshot((snapshot) => {
       sendGrid.send(message)
         .catch((error) => {
           console.error(error);
-          return docRef.update({state: "ERROR", ERROR: error, timeProcessed: admin.firestore.FieldValue.serverTimestamp()});
+          resultBool = true;
+        }).then((data) => {
+          //see download / upload blocks: read data for status
+          resultBool = false;
         });
-      });
+      if(resultBool) 
+      return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: Failed to send email through SendGrid.`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
+      else console.log("No error sendinding email through SendGrid.");
+
+      resultBool = false;
+    });
+    if (resultBool)
+    return docRef.update({'metaData.state': "ERROR", 'metaData.ERROR': `Error: Write stream failed to create PDF: ${outFilePath}`, 'metaData.timeProcessed': admin.firestore.FieldValue.serverTimestamp()});
     
     // updates the state to completed
     console.log("Main thread finished, awaiting async functions.");
-    return docRef.update({ state: "completed" });
+    return docRef.update({ 'metaData.state': "completed" });
 
   });
 });
